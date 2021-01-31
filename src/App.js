4a1b2c8ba525a6e6da50bac2e67b6cf84b06007e
kg-kartik <goel.kartik39@gmail.com>
2021-01-31 19:02:10 +0530

    All set for deployment :rocket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [song, setSong] = useState("");
+    const [result, setResult] = useState("");
+
+    const searchSong = (e) => {
+        setSong(e.target.value);
+
+        if (e.target.value.length > 0) {
+            Axios.post("https://rezo-search.herokuapp.com/search", {
+                query: e.target.value,
+            }).then((response) => {
+                console.log(response.data, "data");
+                setResult(response.data.search_results);
+            });
+        }
+    };
+
+    return (
+        <div>
+            <input type="text" value={song} onChange={(e) => searchSong(e)} />
+            {result && result.map((song) => <h1>{song}</h1>)}
+        </div>
+    );
+};
 
 export default App;

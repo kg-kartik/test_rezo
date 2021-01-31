4767457c25bbc64173ffadfa53b79dca9bacafea
kg-kartik <goel.kartik39@gmail.com>
2021-01-31 19:25:21 +0530

    All set for deployment :rocket:

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ const App = () => {
     const [song, setSong] = useState("");
     const [result, setResult] = useState("");
 
+    const api_url = process.env.REACT_APP_API_URL;
+
     const searchSong = (e) => {
         setSong(e.target.value);
 
         if (e.target.value.length > 0) {
-            Axios.post("https://rezo-search.herokuapp.com/search", {
+            Axios.post(`${api_url}/search`, {
                 query: e.target.value,
             }).then((response) => {
                 console.log(response.data, "data");
@@ -21,7 +23,7 @@ const App = () => {
     return (
         <div>
             <input type="text" value={song} onChange={(e) => searchSong(e)} />
-            {result && result.map((song) => <h1>{song}</h1>)}
+            {result && result.map((song, i) => <h1 key={i}>{song}</h1>)}
         </div>
     );
 };

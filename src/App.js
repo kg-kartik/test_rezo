31ff406eed5bd6e1e9a8d2acc9341f534fb0e412
kg-kartik <goel.kartik39@gmail.com>
2021-02-05 23:12:04 +0530

    All set for deployment :rocket:

--- src/App.js
+++ src/App.js
@@ -1,139 +1,32 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
-import { useEffect } from "react";
-import "../styles/App.css";
+import Axios from "axios";
 
-const Search = () => {
-    const url = process.env.REACT_APP_API_URL;
+const App = () => {
+    const [song, setSong] = useState("");
+    const [result, setResult] = useState("");
 
-    const [search, setSearch] = useState("");
-    const [result, setResult] = useState([]);
-    const [random, setRandom] = useState([]);
+    const api_url = process.env.REACT_APP_API_URL;
 
-    const history = useHistory();
+    const searchSong = (e) => {
+        setSong(e.target.value);
 
-    useEffect(() => {
-        axios.get(url + "random").then((res) => {
-            setRandom(res.data);
-        });
-    }, []);
-
-    const getResults = (e) => {
-        setSearch(e.target.value);
-
-        if (e.target.value.length !== 0) {
-            axios
-                .post(
-                    `${url}/search`,
-                    {
-                        query: e.target.value,
-                    },
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    }
-                )
-                .then((res) => {
-                    console.log(res.data, "data");
-                    setResult(res.data);
-                    setRandom(null);
-                });
+        if (e.target.value.length > 0) {
+            Axios.post(`${api_url}/search`, {
+                query: e.target.value,
+            }).then((response) => {
+                console.log(response.data, "data");
+                setResult(response.data);
+            });
         }
-    };
-
-    const getId = (id, song) => {
-        history.push({
-            pathname: "/recommend",
-            state: { id, song },
-        });
     };
 
     return (
         <div>
-            <div className="main">
-                <div className="button-container">
-                    <input
-                        type="text"
-                        placeholder="Search Songs or Artists"
-                        onChange={(e) => getResults(e)}
-                        value={search}
-                    />
-                    <div className="search"></div>
-                </div>
-            </div>
-
-            {random ? (
-                <div>
-                    <div className="heading-container">
-                        <h1 className="heading-recommend"> Random Songs</h1>
-                    </div>
-                    <div className="container">
-                        <div className="row">
-                            {random.map((songs) => (
-                                <div
-                                    className="col-lg-4 col-md-6 col-sm-6 col-xs-12"
-                                    key={songs.spotify_id}
-                                >
-                                    <div className="profile-card-2">
-                                        <img
-                                            src={songs.image_url}
-                                            alt={songs.song_name}
-                                            className="img img-responsive"
-                                        />
-
-                                        <div className="profile-name">
-                                            {songs.song_name}
-                                        </div>
-                                        <div className="profile-username">
-                                            {songs.artist_name}
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                <div>
-                    <div className="heading-container">
-                        <h1 className="heading-recommend"> Search Results </h1>
-                    </div>
-                    {result && (
-                        <div className="container">
-                            <div className="row">
-                                {result.map((songs) => (
-                                    <div
-                                        className="col-lg-4 col-md-6 col-sm-6 col-xs-12"
-                                        key={songs.ref_id}
-                                    >
-                                        <div className="profile-card-2">
-                                            <img
-                                                src={songs.album_image}
-                                                alt={songs.track_name}
-                                                className="img img-responsive"
-                                                onClick={() =>
-                                                    getId(songs.id, songs.song)
-                                                }
-                                            />
-
-                                            <div className="profile-name">
-                                                {songs.track_name}
-                                            </div>
-                                            <div className="profile-username">
-                                                {songs.artist_name}
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    )}
-                </div>
-            )}
+            <input type="text" value={song} onChange={(e) => searchSong(e)} />
+            {result &&
+                result.map((song, i) => <h1 key={i}>{song.track_name}</h1>)}
         </div>
     );
 };
 
-export default Search;
+export default App;
